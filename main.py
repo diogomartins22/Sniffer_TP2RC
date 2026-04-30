3772b425302a4446d4c6e30a69e0e504433daf68
diogomartins22 <a110702@uminho.pt>
2026-04-30 18:58:36 +0100

    Fiz o filtros todos

--- main.py
+++ main.py
@@ -26,19 +26,6 @@ def detetar_protocolo(pct) -> str:
         return "Ethernet"
     return "Outro"
 
-
-def extrair_caminho_pacote(pct) -> tuple[str, str]:
-    if pct.haslayer(ARP):
-        return str(pct[ARP].psrc), str(pct[ARP].pdst)
-    if pct.haslayer(IP):
-        return str(pct[IP].src), str(pct[IP].dst)
-    if pct.haslayer(IPv6):
-        return str(pct[IPv6].src), str(pct[IPv6].dst)
-    if pct.haslayer(Ether):
-        return str(pct[Ether].src), str(pct[Ether].dst)
-    return "?", "?"
-
-
 def parse():
     parser = argparse.ArgumentParser(
         prog='LarpSniffer',
@@ -46,9 +33,9 @@ def parse():
         epilog='Use --help for more info'
     )
     parser.add_argument('--qnt', type=int, help="número de pacotes")
-    parser.add_argument('--prlt', type=str, help="protocolo a filtrar")
-    parser.add_argument('--ip', type=str, help="filtrar por IP")
-    parser.add_argument('--mac', type=str, help="filtrar por MAC")
+    parser.add_argument('--prtl', type=str, help="protocolo a filtrar")
+    parser.add_argument('--ip',nargs=2, metavar=('IP', 'src/dst'), help="filtrar por IP")
+    parser.add_argument('--mac',nargs=2, metavar=('MAC', 'src/dst'), help="filtrar por MAC")
     parser.add_argument('--log', type=str, help="guardar pacotes num ficheiro")
     parser.add_argument('--live', action='store_true', help="mostrar pacotes no terminal")
 
@@ -56,17 +43,39 @@ def parse():
 
     return args
 
-def printPacote(tempo, iface, size, protocol, src_mac, dst_mac, src_ip, dst_ip, pkt):
-    if IP in pkt:
+def printPacote(tempo, iface, size, protocol, src_mac, dst_mac, src_ip, dst_ip, pkt, ip, mac):
+    #testa se tem --mac primeiro se não tiver dá default para o IP 
+    if mac and Ether in pkt:
+        print(f"{tempo:<20} {src_mac:<20} {dst_mac:<20} {protocol:<10} {size}")
+    elif IP in pkt:
         print(f"{tempo:<20} {src_ip:<20} {dst_ip:<20} {protocol:<10} {size}")
-
-    if Ether in pkt:
+    elif Ether in pkt:
         print(f"{tempo:<20} {src_mac:<20} {dst_mac:<20} {protocol:<10} {size}")
 
 
 
-def sniffer(qnt, prlt, ip, mac, log, live):
-    for i in range(0, qnt):
+def filtro (prtl, ip, mac, protocol, src_mac, dst_mac, src_ip, dst_ip) -> bool:
+    if prtl:
+        if prtl != protocol: return False
+    
+    if ip:
+        if ip[1] == "src":
+            if ip[0] != src_ip:return False
+        elif ip[1] == "dst":
+            if ip[0] != dst_ip:return False
+
+    if mac:
+        if mac[1] == "src":
+            if mac[0] != mac_ip:return False
+        elif mac[1] == "dst":
+            if mac[0] != mac_ip:return False
+    return True
+
+    
+
+def sniffer(qnt, prtl, ip, mac, log, live):
+    i = 0
+    while i < qnt:
         pkts = sniff(count=1)
         pkt = pkts[0]
         #data/hora do pacote
@@ -84,26 +93,27 @@ def sniffer(qnt, prlt, ip, mac, log, live):
         # MAC addresses
         if Ether in pkt:
             src_mac = pkt[Ether].src 
-        else: None
-
+        else: src_mac = "?" 
         if Ether in pkt:
             dst_mac = pkt[Ether].dst
-        else: None
+        else: dst_mac = "?" 
 
 
         # IP addresses
         if IP in pkt:
             src_ip = pkt[IP].src 
-        else: None
+        else: src_ip = "?" 
         if IP in pkt:
             dst_ip = pkt[IP].dst
-        else: None
+        else: dst_ip = "?" 
 
         #falta o resumo do conteúdo (ex.: “ARP request”, “ICMP echo request”, “DHCP Discover”, etc.) depois vê-se
 
         #metemos os filtros no print pacotes acho eu, ou então mal se vê o protocol e isso verifica logo se entra no filtro e
         #para logo o código com break ou o caralho
-        printPacote(tempo, iface, size, protocol, src_mac, dst_mac, src_ip, dst_ip, pkt)
+        if filtro(prtl, ip, mac, protocol, src_mac, dst_mac, src_ip, dst_ip):
+            printPacote(tempo, iface, size, protocol, src_mac, dst_mac, src_ip, dst_ip, pkt, ip, mac)
+            i += 1  # só conta se passar no filtro
 
     
 
@@ -115,7 +125,7 @@ def main() -> int:
         return 0
 
     print(f"{"Tempo":<20} {"Origem":<20} {"Destino":<20} {"Protocolo":<10} {"Tamanho"}")
-    sniffer(args.qnt, args.prlt, args.ip, args.mac, args.log, args.live)
+    sniffer(args.qnt, args.prtl, args.ip, args.mac, args.log, args.live)
 
 
     stop_capture = {"value": False}
